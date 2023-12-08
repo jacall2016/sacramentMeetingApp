755245deab21ef3aa8fa73a740e7cbf088d79304
K-Bambino <59588279+K-Bambino@users.noreply.github.com>
2023-12-07 18:23:44 -0700

    Classes Added

---
+++ sacramentMeetingApp/Models/Unit.cs
@@ -0,0 +1,17 @@
+﻿namespace sacramentMeetingApp.Models
+{
+    public class Unit
+    {
+        String UnitName;
+        Int UnitID;
+        UnitType UnitType;
+        Member[] Leadership;
+    }
+
+    public enum UnitType
+    {
+        Unit,
+        Branch,
+        Ward
+    }
+}
